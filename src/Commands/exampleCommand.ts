8ef153e3b0df94ae47f9758de7fe10fe2c5bdef5
abisammy <minecraftlegolover@gmail.com>
2021-09-09 17:06:59 +0200

    Added examples and defaults

--- src/Commands/exampleCommand.ts
+++ src/Commands/exampleCommand.ts
@@ -1,7 +1,7 @@
 import { Command } from "../Handler";
 
 export const command = new Command();
-command.name = "testing";
+command.name = "example command";
 command.run = (args, message) => {
-    console.log("ASASD");
+    console.log("This is an example command");
 };
