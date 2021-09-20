2c20a2c52a0a0c1135de16d0e941e5d497691f30
abisammy <minecraftlegolover@gmail.com>
2021-09-20 14:11:16 +0200

    Accounted for new util methods

--- src/Commands/exampleCommand.ts
+++ src/Commands/exampleCommand.ts
@@ -1,22 +1,18 @@
-import { GuildMember } from "discord.js";
 import { Command } from "../Handler";
 
 export const command = new Command();
 command.name = "commandName";
 command.description = "commandDescription";
+
+// Add options here
 command.run = async (args, commandRan, commandClass) => {
     const { client } = commandClass;
 
-    const { message, slashCommand } = commandRan;
-
-    const guild = message !== null ? message.guild : slashCommand.guild;
-    const author = message !== null ? message.author : slashCommand.user;
-    const channel = message !== null ? message.channel : slashCommand.channel;
-    const member = message !== null ? message.member : slashCommand.member;
-    const content = message !== null ? message.content : null;
-    const mentions = message !== null ? message.mentions : null;
-
-    if (!(member instanceof GuildMember)) {
-        return false;
-    }
+    const guild = client.utils.getGuild(commandRan);
+    const author = client.utils.getAuthor(commandRan);
+    const channel = client.utils.getChannel(commandRan);
+    const member = client.utils.getMember(commandRan);
+    const content = client.utils.getContent(commandRan);
+    const mentions = client.utils.getMentions(commandRan);
+    // Run code here
 };
