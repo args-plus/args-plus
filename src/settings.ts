e303aee04fa5e45f11ca8adba77c1e5c07798ae0
abisammy <minecraftlegolover@gmail.com>
2021-10-27 09:24:40 +0200

    Fixed default settings

--- src/settings.ts
+++ src/settings.ts
@@ -1,15 +1,3 @@
 import { ClientConfig } from "./Handler/Client/config";
-const settings = new ClientConfig("one");
-settings.loadGuildSlashCommands = true;
-settings.slashCommandGuilds = ["742308480299565086"];
-settings.botDevelopers = ["468128787884670986"];
-settings.indentMessageContent = false;
-settings.messagesOrEmbeds = "embeds";
-settings.mainColor = "RANDOM";
-// settings.blacklistedGuilds = [
-// { id: "742308480299565086", reason: "Very bas" },
-// { id: "asd" },
-// { id: "dsa", reason: "VVVV" },
-// ];
-// settings.blacklistedUsers = [{ id: "468128787884670986" }];
+const settings = new ClientConfig("ts!");
 export default settings;
