73668a51861e31c208134f0253e3bfa5bfb384aa
abisammy <minecraftlegolover@gmail.com>
2021-10-28 12:22:15 +0200

    Fixed exports

--- src/Handler/index.ts
+++ src/Handler/index.ts
@@ -1,3 +1,5 @@
 export { ExtendedClient as Client, ClientConfig } from "./Client";
 export { Command, PreCommandFunction, PostCommandFunction } from "./Commands";
 export { Event } from "./Events";
+export { Check } from "./Checks";
+export { Extension } from "./Extensions";
