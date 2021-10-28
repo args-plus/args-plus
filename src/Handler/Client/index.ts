509c6883004b6a02269dd3d01da45fbc48f8a55d
abisammy <minecraftlegolover@gmail.com>
2021-10-28 12:23:40 +0200

    Fixed not loading custom extensions

--- src/Handler/Client/index.ts
+++ src/Handler/Client/index.ts
@@ -131,7 +131,8 @@ export class ExtendedClient extends Client {
         await items.registerSlashCommands();
 
         await items.loadItems(this.extensions, Extension, [
-            "/Handler/Defaults/Extensions"
+            "/Handler/Defaults/Extensions",
+            "/Extensions"
         ]);
 
         await items.runExtensions();
