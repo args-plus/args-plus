4b9c4453dff144b5aeee6142be702be44baaf2a1
abisammy <minecraftlegolover@gmail.com>
2021-12-20 14:39:41 +0100

    Update categories of default commands

---
+++ src/Commands/Miscelanious/category.ts
@@ -0,0 +1,6 @@
+import { Category } from "../../Handler";
+
+const category = new Category("General");
+category.description = "General commands that can be ran";
+
+export default category;
---
+++ src/Commands/Prefixes/category.ts
@@ -0,0 +1,6 @@
+import { Category } from "../../Handler";
+
+const category = new Category("Prefixes");
+category.description = "Commands to do with prefixes";
+
+export default category;
