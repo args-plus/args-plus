76d6cd9a75dc9a758f979aa77f70716bf6cf9556
abisammy <minecraftlegolover@gmail.com>
2021-12-21 22:36:55 +0100

    Update description

--- src/Commands/Developers/category.ts
+++ src/Commands/Developers/category.ts
@@ -1,7 +1,7 @@
 import { Category } from "../../Handler";
 
 const category = new Category("Developers");
-category.description = "Commands that can be ran by bot developers";
+category.description = "Commands that can only be ran by bot developers";
 category.developerOnly = true;
 category.overideLoadSlashCommand = true;
 
