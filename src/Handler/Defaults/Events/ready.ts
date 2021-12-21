d2ec0102143bc319da6c7bf9494a23a17aa40db7
abisammy <minecraftlegolover@gmail.com>
2021-12-21 19:46:30 +0100

    Exported event correctly

--- src/Handler/Defaults/Events/ready.ts
+++ src/Handler/Defaults/Events/ready.ts
@@ -6,3 +6,5 @@ event.run = (client) => {
     if (client.user) client.console.log(`Logged into ${client.user.username}`);
     else console.log(`Logged into discord.js`);
 };
+
+export default event;
