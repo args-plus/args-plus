44cd227b1994231f08ed83134943ff22e2120879
abisammy <minecraftlegolover@gmail.com>
2021-12-22 09:42:59 +0100

    Add setters to commands

--- src/Handler/Commands/index.ts
+++ src/Handler/Commands/index.ts
@@ -10,36 +10,36 @@ import { ReturnCommand } from "./returnCommand";
 export class Command extends Item {
     public args: Argument[] = [];
 
-    public description = "";
-    public aliases: string[] = []; // DONE
-    public hiddenAliases: string[] = []; // DONE
-    public developerOnly = false; // DONE
-    public guildOnly = false; // DONE
-    public deferResponse = true; // DONE
-    public certainChannelsOnly: string[] = []; // DONE
-    public certainGuildsOnly: string[] = []; // DONE
-    public certainRolesOnly: string[] = []; // DONE
-    public overideLoadSlashCommand = false; // DONE
-    public overideDefaultClientChecks = false; // DONE
-    public overideDefaultUserChecks = false; // DONE
-    public overideDefaultUserPermissions = false; // DONE
-    public overideDefaultClientPermissions = false; // DONE
-    public overideGuildBlacklist = false; // DONE
-    public overideUserBlacklist = false; // DONE
+    public description = "This command has no description";
+    public aliases: string[] = [];
+    public hiddenAliases: string[] = [];
+    public developerOnly = false;
+    public guildOnly = false;
+    public deferResponse = true;
+    public certainChannelsOnly: string[] = [];
+    public certainGuildsOnly: string[] = [];
+    public certainRolesOnly: string[] = [];
+    public overideLoadSlashCommand = false;
+    public overideDefaultClientChecks = false;
+    public overideDefaultUserChecks = false;
+    public overideDefaultUserPermissions = false;
+    public overideDefaultClientPermissions = false;
+    public overideGuildBlacklist = false;
+    public overideUserBlacklist = false;
     public overideAutoRemove = false;
-    public overideConstraints = false; // DONE
-    public clientPermissions: Permission[] = []; // DONE
-    public userPermissions: Permission[] = []; // DONE
-    public clientChecks: string[] = []; // DONE
-    public userChecks: string[] = []; // DONE
+    public overideConstraints = false;
+    public clientPermissions: Permission[] = [];
+    public userPermissions: Permission[] = [];
+    public clientChecks: string[] = [];
+    public userChecks: string[] = [];
     public hidden = false;
-    public category: [string, string] | string | false = ""; // DONE
-    public categoryName: string | false = ""; // DONE
+    public category: [string, string] | string | false = "";
+    public categoryName: string | false = "";
     public categoryId: string | false = "";
     public toggleable = true;
 
-    public usage: string[] = []; // DONE
-    public examples: (string[] | [number, number, boolean])[] = []; // DONE
+    public usage: string[] = [];
+    public examples: (string[] | [number, number, boolean])[] = [];
 
     public activeCooldowns: Collection<string, number[]> = new Collection();
 
@@ -47,13 +47,173 @@ export class Command extends Item {
 
     private loadedSlashCommand = false;
 
+    public setDescription(description: string = "This command has no description") {
+        this.description = description;
+        return this;
+    }
+
+    public setAliases(aliases: string[] = []) {
+        this.aliases = aliases;
+        return this;
+    }
+
+    public setHiddenAliases(aliases: string[] = []) {
+        this.hiddenAliases = aliases;
+        return this;
+    }
+
+    public setAllAliases(aliases: string[] = [], hiddenAliases: string[] = []) {
+        this.setAliases(aliases);
+        this.setHiddenAliases(hiddenAliases);
+        return this;
+    }
+
+    public setDeveloperOnly(developerOnly = false) {
+        this.developerOnly = developerOnly;
+        return this;
+    }
+
+    public setGuildOnly(guildOnly = false) {
+        this.guildOnly = guildOnly;
+        return this;
+    }
+
+    public setDefer(defer = true) {
+        this.deferResponse = defer;
+        return this;
+    }
+
+    public setCertainChannelsOnly(channels: string[] = []) {
+        this.certainChannelsOnly = channels;
+        return this;
+    }
+
+    public setCertainGuildsOnly(guilds: string[] = []) {
+        this.certainChannelsOnly = guilds;
+        return this;
+    }
+
+    public setCerainrolesOnly(roles: string[] = []) {
+        this.certainRolesOnly = roles;
+        return this;
+    }
+
+    public setOverideLoadSlashCommands(overide = true) {
+        this.overideLoadSlashCommand = overide;
+        return this;
+    }
+
+    public setOverideDefaultClientChecks(checks = true) {
+        this.overideDefaultClientChecks = checks;
+        return this;
+    }
+
+    public setOverideDefaultUserChecks(checks = true) {
+        this.overideDefaultUserChecks = checks;
+        return this;
+    }
+
+    public setOverideChecks(client = true, user = true) {
+        this.setOverideDefaultClientChecks(client);
+        this.setOverideDefaultUserChecks(user);
+        return this;
+    }
+
+    public setOverideDefaultClientPermisions(client = true) {
+        this.overideDefaultClientPermissions = client;
+        return this;
+    }
+
+    public setOverideDefaultUserPermissions(user = true) {
+        this.overideDefaultUserPermissions = user;
+        return this;
+    }
+
+    public setOverideDefaultPermissions(client = true, user = true) {
+        this.setOverideDefaultClientPermisions(client);
+        this.setOverideDefaultUserPermissions(user);
+        return this;
+    }
+
+    public setOverideGuildBlacklist(guild = true) {
+        this.overideGuildBlacklist = guild;
+        return this;
+    }
+
+    public setOverideUserBlacklist(user = true) {
+        this.overideUserBlacklist = user;
+        return this;
+    }
+
+    public setOverideBlacklist(guild = true, user = true) {
+        this.setOverideGuildBlacklist(guild);
+        this.setOverideUserBlacklist(user);
+        return this;
+    }
+
+    public setOverideAutoRemove(auto = true) {
+        this.overideAutoRemove = auto;
+        return this;
+    }
+
+    public setOverideConstratins(constratints = true) {
+        this.overideConstraints = constratints;
+        return this;
+    }
+
+    public setClientPermissions(permissions: Permission[] = []) {
+        this.clientPermissions = permissions;
+        return this;
+    }
+
+    public setUserPermissions(permissions: Permission[] = []) {
+        this.userPermissions = permissions;
+        return this;
+    }
+
+    public setPermissions(client: Permission[] = [], user: Permission[] = []) {
+        this.setClientPermissions(client);
+        this.setUserPermissions(user);
+        return this;
+    }
+
+    public setClientChecks(client: string[] = []) {
+        this.clientChecks = client;
+        return this;
+    }
+
+    public setUserChecks(user: string[] = []) {
+        this.userChecks = user;
+        return this;
+    }
+
+    public setChecks(client: string[] = [], user: string[] = []) {
+        this.setClientChecks(client);
+        this.setUserChecks(user);
+        return this;
+    }
+
+    public setHidden(hidden = true) {
+        this.hidden = hidden;
+        return this;
+    }
+
+    public setCategory(category: [string, string] | string | false = false) {
+        this.category = category;
+        return this;
+    }
+
+    public setToggleable(toggleable = false) {
+        this.toggleable = toggleable;
+        return this;
+    }
+
     public getLoadedSlashCommand() {
         return this.loadedSlashCommand;
     }
 
     public setLoadedSlashCommand() {
         this.loadedSlashCommand = true;
-        return true;
     }
 
     public getCooldownNumber() {
@@ -62,11 +222,23 @@ export class Command extends Item {
 
     public run: (client: Client, returnCommand: ReturnCommand) => any = () => {};
 
-    // prettier-ignore
-    public setCooldown(cooldownKey:  [`${string}s`| `${string}m` | `${string}h`| `${string}d`| `${string}m ${string}s`| `${string}h ${string}m`| number, number] = [0, 0]){
-        const cooldown = cooldownKey[0]
-        
-        let ms = 0
+    public setCooldown(
+        cooldownKey: [
+            (
+                | `${string}s`
+                | `${string}m`
+                | `${string}h`
+                | `${string}d`
+                | `${string}m ${string}s`
+                | `${string}h ${string}m`
+                | number
+            ),
+            number
+        ] = [0, 0]
+    ) {
+        const cooldown = cooldownKey[0];
+
+        let ms = 0;
 
         if (typeof cooldown === "string") {
             if (cooldown.endsWith("s")) {
@@ -127,12 +299,13 @@ export class Command extends Item {
                     const minNumber = exampleKey[0];
                     const maxNumber = exampleKey[1];
 
-                    // prettier-ignore
-                    let randomNumber = Math.random() * (maxNumber - minNumber) + minNumber;
+                    let randomNumber =
+                        Math.random() * (maxNumber - minNumber) + minNumber;
 
                     if (!exampleKey[2]) {
-                        // prettier-ignore
-                        currentExample += (Math.round((randomNumber + Number.EPSILON) * 100) / 100).toString();
+                        currentExample += (
+                            Math.round((randomNumber + Number.EPSILON) * 100) / 100
+                        ).toString();
                     } else {
                         currentExample += Math.floor(randomNumber).toString();
                     }
