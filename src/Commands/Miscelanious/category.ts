3d48fd9d9e23eb3a8b8750a250dbd5bf889b1fdc
abisammy <minecraftlegolover@gmail.com>
2021-12-22 12:31:21 +0100

    Update to use setters

--- src/Commands/Miscelanious/category.ts
+++ src/Commands/Miscelanious/category.ts
@@ -1,6 +1,7 @@
 import { Category } from "../../Handler";
 
-const category = new Category("General");
-category.description = "General commands that can be ran";
+const category = new Category("General").setDescription(
+    "General commands that can be ran"
+);
 
 export default category;
